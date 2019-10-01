9b2cd45de5284e03b03be41ea5f9e63a5503a0c0
Georgy <rybakovgeorg@gmail.com>
2019-10-01 18:23:29 +0300

    add vcr tests

--- apidatamosru/apidatamos.py
+++ apidatamosru/apidatamos.py
@@ -5,15 +5,15 @@ from .exceptions import APIDataMosError
 
 class ApiClient(object):
 
-    def __init__(self, app_key, timeout=10):
+    def __init__(self, api_key, timeout=10):
 
         self.base_api_url = 'https://apidata.mos.ru'
         self.api_version = 'v1'
         self.api_url = '{0}/{1}'.format(self.base_api_url, self.api_version)
 
-        if not app_key:
+        if not api_key:
             raise Exception('API key is required.')
-        self.app_key = app_key
+        self.api_key = api_key
 
         if not isinstance(timeout, (int, float)):
             raise TypeError('Timeout must be numeric.')
@@ -22,52 +22,81 @@ class ApiClient(object):
         self.client = requests.Session()
 
     def get_datasets_list(self, **kwargs):
-        kwargs['app_key'] = self.app_key
+
+        # https://apidata.mos.ru/Docs#datasetsList
+
+        params = {
+            '$top': kwargs.pop('top', None),
+            '$skip': kwargs.pop('skip', None),
+            '$inlinecount': kwargs.pop('inlinecount', None),
+            '$orderby': kwargs.pop('orderby', None),
+            '$filter': kwargs.pop('filter', None),
+            'foreign': kwargs.pop('foreign', None)
+        }
+        params.update(kwargs)
+        params = _clear_empty_values(params)
+        params['api_key'] = self.api_key
+
         endpoint = '{0}/{1}/datasets'.format(self.base_api_url, self.api_version)
-        return self._request(endpoint, params=kwargs)
+        return self._request(endpoint, params=params)
 
     def get_dataset_meta(self, id, **kwargs):
         self._check_id(id)
-        kwargs['app_key'] = self.app_key
+        kwargs['api_key'] = self.api_key
         endpoint = '{0}/{1}/datasets/{2}'.format(self.base_api_url, self.api_version, str(id))
         return self._request(endpoint, params=kwargs)
 
     def get_dataset_count(self, id, **kwargs):
         self._check_id(id)
-        kwargs['app_key'] = self.app_key
+        kwargs['api_key'] = self.api_key
         endpoint = '{0}/{1}/datasets/{2}/count'.format(self.base_api_url, self.api_version, str(id))
         return self._request(endpoint, params=kwargs)
 
     def get_dataset_version(self, id, **kwargs):
         self._check_id(id)
-        kwargs['app_key'] = self.app_key
+        kwargs['api_key'] = self.api_key
         endpoint = '{0}/{1}/datasets/{2}/version'.format(self.base_api_url, self.api_version, str(id))
         return self._request(endpoint, params=kwargs)
 
     def get_dataset_icons(self, id, size='m', **kwargs):
         # todo Пересечение
         self._check_id(id)
-        kwargs['app_key'] = self.app_key
+        kwargs['api_key'] = self.api_key
         endpoint = '{0}/{1}/datasets/{2}/icon/{3}'.format(self.base_api_url, self.api_version, str(id), size)
         return self._request(endpoint, params=kwargs)
 
     def get_dataset_image(self, id, width=70, **kwargs):
         self._check_id(id)
-        kwargs['app_key'] = self.app_key
+        kwargs['api_key'] = self.api_key
         endpoint = '{0}/{1}/datasets/{2}/image/{3}'.format(self.base_api_url, self.api_version, str(id), width)
         return self._request(endpoint, params=kwargs)
 
     def get_dataset_marker(self, id, **kwargs):
         self._check_id(id)
-        kwargs['app_key'] = self.app_key
+        kwargs['api_key'] = self.api_key
         endpoint = '{0}/{1}/datasets/{2}/marker'.format(self.base_api_url, self.api_version, str(id))
         return self._request(endpoint, params=kwargs)
 
     def get_dataset(self, id, **kwargs):
         self._check_id(id)
-        kwargs['app_key'] = self.app_key
+
+        # https://apidata.mos.ru/Docs#datasetRows
+
+        params = {
+            '$top': kwargs.pop('top', None),
+            '$skip': kwargs.pop('skip', None),
+            '$orderby': kwargs.pop('orderby', None),
+            '$filter': kwargs.pop('filter', None),
+            'versionNumber': kwargs.pop('versionNumber', None),
+            'releaseNumber': kwargs.pop('releaseNumber', None),
+            'q': kwargs.pop('q', None)
+        }
+        params.update(kwargs)
+        params = _clear_empty_values(params)
+        params['api_key'] = self.api_key
+
         endpoint = '{0}/{1}/datasets/{2}/rows'.format(self.base_api_url, self.api_version, str(id))
-        return self._request(endpoint, params=kwargs)
+        return self._request(endpoint, params=params)
 
         # todo Проекция данных
         # Для указанного метода доступна возможность проекции данных.
@@ -75,9 +104,19 @@ class ApiClient(object):
 
     def get_geodata(self, id, **kwargs):
         self._check_id(id)
-        kwargs['app_key'] = self.app_key
+
+        # https://apidata.mos.ru/Docs#datasetFeatures
+
+        params = {
+            'versionNumber': kwargs.pop('versionNumber', None),
+            'releaseNumber': kwargs.pop('releaseNumber', None)
+        }
+        params.update(kwargs)
+        params = _clear_empty_values(params)
+        params['api_key'] = self.api_key
+
         endpoint = '{0}/{1}/datasets/{2}/features'.format(self.base_api_url, self.api_version, str(id))
-        return self._request(endpoint, params=kwargs)
+        return self._request(endpoint, params=params)
 
         # todo Проекция данных
         # Для указанного метода доступна возможность проекции данных.
@@ -96,4 +135,15 @@ class ApiClient(object):
         if response.status_code != 200:
             raise APIDataMosError('{0} {1}'.format(response.status_code, response.reason), response.status_code)
         return response
+
+def _clear_empty_values(args):
+    '''
+    Original: https://github.com/xmunoz/sodapy/blob/master/sodapy/__init__.py
+    Scrap junk data from a dict.
+    '''
+    result = {}
+    for param in args:
+        if args[param] is not None:
+            result[param] = args[param]
+    return result
 
--- tests/test_methods.py
+++ tests/test_methods.py
@@ -7,20 +7,38 @@ from apidatamosru.exceptions import APIDataMosError, APIDataMosCountLimitError, 
 
 API_KEY = '7e5b3dd05fbfe77c66a8c356c45eea81'
 
-@vcr.use_cassette('test-invalid_api_key.yml')
-@pytest.mark.parametrize('api_key', [
-        (''),
-        ('31337'),
-        ('dreams'),
-        ('d870b8ec9ae96da2f33493f85f01999f'),
-        ('-1'),
-        ('31337.31337'),
-    ])
-def test_invalid_api_key(api_key):
+
+@vcr.use_cassette()
+def test_of_datasets_list():
+    client = ApiClient(API_KEY)
+    response = client.get_datasets_list()
+    assert (response.status_code == 200)
+    assert (response.json() is not None)
+
+@vcr.use_cassette()
+def test_count_datasets_list():
+    client = ApiClient(API_KEY)
+    response = client.get_datasets_list(inlinecount='allpages')
+    assert (response.status_code == 200)
+    assert ('Items' in response.json())
+    assert ('Count' in response.json())
+
+@vcr.use_cassette()
+def test_top_datasets_list():
+    client = ApiClient(API_KEY)
+    response = client.get_datasets_list(top=6)
+    assert (response.status_code == 200)
+    assert (len(response.json()) == 6)
+
+# todo
+
+@vcr.use_cassette()
+def test_invalid_api_key():
+    api_key = 'd870b8ec9ae96da2f33493f85f01999f'
 
     client = ApiClient(api_key, 8)
-    with pytest.raises(APIDataMosCountLimitError):
+    with pytest.raises(APIDataMosAuthError):
         response = client.get_datasets_list()
         assert (response.status_code == 403)
-        # assert (response.json()['Items'])
+        assert (response.json() is None)
 
